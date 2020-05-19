39b2063b8f053c8f7627dbb085be85c4a3d947b2
Nacho-Keiniger <37520232+Nacho-Keiniger@users.noreply.github.com>
2020-05-19 19:27:31 -0300

    Update Program.cs
    le puse valores a los arrays de los inicializadores

--- Tarea Organizaciones/definirEstructura/Program.cs
+++ Tarea Organizaciones/definirEstructura/Program.cs
@@ -44,20 +44,12 @@ internal class Actividad
     public float[] PromedioVentasMax { get => promedioVentasMax; set => promedioVentasMax = value; }
 }
 
-internal class Comercio : Actividad
-{
-    public Comercio() {
-        CantidadPersonalMax = new int[4] { };
-        PromedioVentasMax = new float[4] { };
-    }
-}
-
 internal class Construccion : Actividad
 {
     public Construccion()
     {
         CantidadPersonalMax = new int[4] { 12, 45, 200, 590};
-        PromedioVentasMax = new float[4] { };
+        PromedioVentasMax = new float[4] { 15230000, 90310000, 503880000, 755740000};
     }
 }
 
@@ -65,17 +57,24 @@ internal class Servicios : Actividad
 {
     public Servicios()
     {
-        CantidadPersonalMax = new int[4] { };
-        PromedioVentasMax = new float[4] { };
+        CantidadPersonalMax = new int[4] { 7, 30, 165, 535 };
+        PromedioVentasMax = new float[4] { 8500000, 50950000, 425170000, 607210000};
     }
 }
 
+internal class Comercio : Actividad
+{
+    public Comercio() {
+        CantidadPersonalMax = new int[4] { 7, 35, 125, 345};
+        PromedioVentasMax = new float[4] { 29740000, 178860000, 1502750000, 2146810000};
+    }
+}
 internal class IndustriaMineria: Actividad
 {
     public IndustriaMineria()
     {
-        CantidadPersonalMax = new int[4] { };
-        PromedioVentasMax = new float[4] { };
+        CantidadPersonalMax = new int[4] { 15, 60, 235, 655};
+        PromedioVentasMax = new float[4] { 25540000, 190410000, 1190330000, 1739590000};
     }
 }
 
@@ -83,7 +82,7 @@ internal class Agropecuario : Actividad
 {
     public Agropecuario()
     {
-        CantidadPersonalMax = new int[4] { };
-        PromedioVentasMax = new float[4] { };
+        CantidadPersonalMax = new int[4] { 5, 10, 50, 215};
+        PromedioVentasMax = new float[4] { 12890000, 48480000, 345430000, 547890000};
     }
 }
