d2d8cab506c7b6613fc1925571dcf2701bbcfa9c
Ignacio Keiniger <nkeiniger@gmail.com>
2020-07-11 20:03:33 -0300

    implementacion ej 79

--- Ejercicio 79/Package1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Ejercicio 79/Package1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,10 +4,32 @@ namespace InteresesBancarios
 {
     class Program
     {
-        TarjetaDeCrédito tdc = new TarjetaDeCrédito(150);
-        CajaDeAhorro cda = new CajaDeAhorro(260);
-        CuentaCorriente cc = new CuentaCorriente(300);
+        static void Main(string[] args)
+        {
+            TarjetaDeCrédito tdc = new TarjetaDeCrédito(150);
+            CuentaCorriente cc = new CuentaCorriente(300);
+            CajaDeAhorro cda = new CajaDeAhorro(260);
+            Interés i = new Interés();
 
+            Console.WriteLine(tdc.Dinero);
+            Console.WriteLine(cc.Dinero);
+            Console.WriteLine(cda.Dinero);
+            
+            /* 
+            
+            i.visit(tdc);
+            i.visit(cc);
+            i.visit(cda);
+            
+            */
+            
+            tdc.accept(i);
+            cc.accept(i);
+            cda.accept(i);
 
+            Console.WriteLine(tdc.Dinero);
+            Console.WriteLine(cc.Dinero);
+            Console.WriteLine(cda.Dinero);
+        }
     }
 }
