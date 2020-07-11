7412814e2a0e43ff1f27cd59926a0c0d349f1ce7
Ignacio Keiniger <nkeiniger@gmail.com>
2020-07-11 19:22:56 -0300

    Codigo ej 79

---
+++ Ejercicio 79/Package1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InteresesBancarios
+{
+    class Program
+    {
+        TarjetaDeCrédito tdc = new TarjetaDeCrédito(150);
+        CajaDeAhorro cda = new CajaDeAhorro(260);
+        CuentaCorriente cc = new CuentaCorriente(300);
+
+
+    }
+}
