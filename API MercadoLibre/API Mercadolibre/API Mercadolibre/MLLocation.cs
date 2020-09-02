a93d8ae8c0e70d21091021e938b9bfccce8aba7f
Nacho-Keiniger <37520232+Nacho-Keiniger@users.noreply.github.com>
2020-09-01 21:31:30 -0300

    Llena info de la API a clases especiales de ML

---
+++ API MercadoLibre/API Mercadolibre/API Mercadolibre/MLLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Mercadolibre
+{
+    public class MLLocation
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+}
